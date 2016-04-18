8a8ec24a36075d417d0513ef11c72c9b24ae0e96
Will Jordan <will@code.org>
2016-04-18 13:26:03 -0700

    add checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 
 default['ixgbevf']['version'] = "3.1.2"
+default['ixgbevf']['checksum'] = "845f37a16a5acc491b5e6bdf6d3fe580724d82b7cf88ac080e3c8f9ceeea79ee"
 default['ixgbevf']['package'] = "ixgbevf-#{node['ixgbevf']['version']}.tar.gz"
 default['ixgbevf']['package_url'] = "http://sourceforge.net/projects/e1000/files/ixgbevf stable/#{node['ixgbevf']['version']}/#{node['ixgbevf']['package']}"
 default['ixgbevf']['dir']     = "/usr/src/ixgbevf-#{node['ixgbevf']['version']}"
